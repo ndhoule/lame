ed4bcbc3b183d0937d30e0f7f9f7e8eb128d3409
Nathan Houle <nathan@nathanhoule.com>
2014-09-09 19:47:47 -0700

    Add explanatory note to `bootstrap.js`

--- bootstrap.js
+++ bootstrap.js
@@ -1,5 +1,13 @@
 'use strict';
 
+/**
+ * This file bootstraps Traceur (ES6) into Node's module system. It'll interpret any `require`d or
+ * `import`ed files as ES6 modules; any packages installed in the `node_modules` folder will be
+ * interpreted as normal ES5 files.
+ *
+ * https://github.com/google/traceur-compiler/wiki/Using-Traceur-with-Node.js
+ */
+
 var traceur = require('traceur');
 
 traceur.require.makeDefault(function(filename) {
