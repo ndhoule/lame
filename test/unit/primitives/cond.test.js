1e33ac3b523e7fb73f18e8f977a538c04f5c4b58
Nathan Houle <nathan@nathanhoule.com>
2014-09-12 16:16:25 -0700

    Clean up `cond` tests

--- test/unit/primitives/cond.test.js
+++ test/unit/primitives/cond.test.js
@@ -1,17 +1,25 @@
 import lame from '../../../src/index';
 
-describe('primitives » cond', function() {
+describe('core » s-functions » cond', function() {
   it('should return the first clause\'s `expr` for which the `test` evaluates to truthy', function() {
     var simple = lame.eval(chai.loadFixture('cond/simple'));
     var simpleExpected = '"first"';
-    var complex = lame.eval(chai.loadFixture('cond/complex'));
-    var complexExpected = '"third"';
-    var fallback = lame.eval(chai.loadFixture('cond/fallback'));
-    var fallbackExpected = '"last"';
 
     expect(simple).to.equal(simpleExpected);
-    expect(complex).to.equal(complexExpected);
-    expect(fallback).to.equal(fallbackExpected);
+  });
+
+  it('should work when given multiple clauses', function() {
+    var code1 = chai.loadFixture('cond/complex');
+    var results1 = lame.eval(code1);
+    var expected1 = '"third"';
+
+    expect(results1).to.equal(expected1);
+
+    var code2 = chai.loadFixture('cond/fallback');
+    var results2 = lame.eval(code2);
+    var expected2 = '"last"';
+
+    expect(results2).to.equal(expected2);
   });
 
   it('should return `nil` if none of the clause tests evaluate to a truthy value', function() {
