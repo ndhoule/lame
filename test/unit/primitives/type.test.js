0d721d648568aaf11f22c73caa6d6c14edfd5721
Nathan Houle <nathan@nathanhoule.com>
2014-09-12 17:47:42 -0700

    Add tests for `type` function

---
+++ test/unit/primitives/type.test.js
@@ -0,0 +1,55 @@
+import lame from '../../../src/index';
+
+describe('core » s-functions » type', function() {
+  it('should return a value of type `string`', function() {
+    var code = '(type (type 1))';
+    var result = lame.eval(code);
+    var expected = 'string';
+
+    expect(result).to.equal(expected);
+  });
+
+  it('should return the type from symbol wrappers', function() {
+    var code = '(type \'x)';
+    var result = lame.eval(code);
+    var expected = 'symbol';
+
+    expect(result).to.equal(expected);
+  });
+
+  it('should return the type from number wrappers', function() {
+    var code = '(type 1)';
+    var result = lame.eval(code);
+    var expected = 'number';
+
+    expect(result).to.equal(expected);
+  });
+
+  it('should return the type from string wrappers', function() {
+    var code = '(type "test")';
+    var result = lame.eval(code);
+    var expected = 'string';
+
+    expect(result).to.equal(expected);
+  });
+
+  it('should return the type from `nil` wrappers', function() {
+    var code = '(type nil)';
+    var result = lame.eval(code);
+    var expected = 'nil';
+
+    expect(result).to.equal(expected);
+  });
+
+  it('should return the type from boolean wrappers', function() {
+    var code = '(type false)';
+    var result = lame.eval(code);
+    var expected = 'boolean';
+
+    expect(result).to.equal(expected);
+  });
+
+  xit('should return the type from list wrappers', function() {});
+
+  xit('should return the type from function wrappers', function() {});
+});
