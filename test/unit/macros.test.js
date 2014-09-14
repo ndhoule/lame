7afbfa3808f3bd140d588e14ef0fe3149c1796f6
Nathan Houle <nathan@nathanhoule.com>
2014-09-14 15:17:13 -0700

    Move tests

--- test/unit/macros.test.js
+++ test/unit/macros.test.js
@@ -1,7 +1,30 @@
 import lame from '../../src/index';
 
 describe('syntax', function() {
-  describe('quasiquotes', function() {
+  describe('\' (`quote` shorthand)', function() {
+    it('should quote an atom', function() {
+      var result = lame.eval('\'+');
+      var expected = '+';
+
+      expect(result).to.deep.equal(expected);
+    });
+
+    it('should prevent evaluation of an s-expression', function() {
+      var result = lame.eval(chai.loadFixture('quote/shorthand-single'));
+      var expected = ['+', 2, 3];
+
+      expect(result).to.deep.equal(expected);
+    });
+
+    it('should prevent evaluation of nested s-expressions', function() {
+      var result = lame.eval(chai.loadFixture('quote/shorthand-nested'));
+      var expected = ['list', 2, 3, ['quote', [4, 5]], 6];
+
+      expect(result).to.deep.equal(expected);
+    });
+  });
+
+  describe('quasiquotes (`)', function() {
     it('should rewrite quasiquotes on atoms', function() {
       expect(lame.eval('`+')).to.equal('+');
     });
@@ -11,7 +34,7 @@ describe('syntax', function() {
     });
   });
 
-  describe('unquote', function() {
+  describe('unquote (~)', function() {
     it('should enable evaluation of unquoted forms within a quasiquoted form', function() {
       expect(lame.eval('`~(+ 1 2)')).to.equal(3);
     });
@@ -21,7 +44,7 @@ describe('syntax', function() {
     });
   });
 
-  describe('unquote-splicing', function() {
+  describe('unquote-splicing (~@)', function() {
     it('should throw an error when attempting to unquote splice immediately after a quasiquote', function() {
       expect(() => lame.eval('`~@(list 1 2 3)')).to.throw(SyntaxError);
     });
--- test/unit/primitives/quote.test.js
+++ test/unit/primitives/quote.test.js
@@ -22,26 +22,3 @@ describe('core » s-functions » quote', function() {
     expect(result).to.deep.equal(expected);
   });
 });
-
-describe('\' (`quote` shorthand)', function() {
-  it('should quote an atom', function() {
-    var result = lame.eval('\'+');
-    var expected = '+';
-
-    expect(result).to.deep.equal(expected);
-  });
-
-  it('should prevent evaluation of an s-expression', function() {
-    var result = lame.eval(chai.loadFixture('quote/shorthand-single'));
-    var expected = ['+', 2, 3];
-
-    expect(result).to.deep.equal(expected);
-  });
-
-  it('should prevent evaluation of nested s-expressions', function() {
-    var result = lame.eval(chai.loadFixture('quote/shorthand-nested'));
-    var expected = ['list', 2, 3, ['quote', [4, 5]], 6];
-
-    expect(result).to.deep.equal(expected);
-  });
-});
