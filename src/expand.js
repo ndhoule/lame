cd5da12ccbeae1b69783060d8f4d6e45d86fd7e1
Nathan Houle <nathan@nathanhoule.com>
2014-09-18 13:05:06 -0700

    Minor file cleanups

--- src/expand.js
+++ src/expand.js
@@ -2,20 +2,8 @@ import { concat, cons } from './core';
 import { LAtom, LSymbol } from './types';
 import { _quasiquote, _unquote, _unquoteSplice } from './symbols';
 
-var expand = function(x) {
-  if (!Array.isArray(x)) {
-    return x;
-  }
-
-  if (x.length === 0) {
-    return x;
-  }
-
-  if (LAtom.equal(x[0], _quasiquote)) {
-    return expandQuasiquote(x[1]);
-  }
-
-  return x.map(expand);
+var isPair = function(x) {
+  return Array.isArray(x) && x.length !== 0;
 };
 
 var expandQuasiquote = function(x) {
@@ -38,8 +26,20 @@ var expandQuasiquote = function(x) {
   }
 };
 
-var isPair = function(x) {
-  return Array.isArray(x) && x.length !== 0;
+var expand = function(x) {
+  if (!Array.isArray(x)) {
+    return x;
+  }
+
+  if (x.length === 0) {
+    return x;
+  }
+
+  if (LAtom.equal(x[0], _quasiquote)) {
+    return expandQuasiquote(x[1]);
+  }
+
+  return x.map(expand);
 };
 
 export default expand;
